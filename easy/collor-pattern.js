26f9103a8c295dd605065867e21b219698eff92d
Gabriel Oliveira <52364235+srgaba@users.noreply.github.com>
2022-02-05 17:16:46 -0300

    Create collor-pattern.js

---
+++ easy/collor-pattern.js
@@ -0,0 +1,11 @@
+//https://edabit.com/challenge/Np7R8F24PaqBShZc5
+
+const colorPatternTimes = (cols) => {
+  const colored = cols.length * 2
+  const switched = cols.reduce((total, value, i, values) => {
+    const next = values[i+1]
+    if(next) if(value !== values[i+1]) total += 1
+    return total
+  }, 0)
+  return colored + switched
+}
