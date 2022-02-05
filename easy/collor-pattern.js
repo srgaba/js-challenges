ef4c45651c7e6711611df5b4f0a7fb6c76576776
Gabriel Oliveira <52364235+srgaba@users.noreply.github.com>
2022-02-05 17:17:08 -0300

    Update collor-pattern.js

--- easy/collor-pattern.js
+++ easy/collor-pattern.js
@@ -4,7 +4,7 @@ const colorPatternTimes = (cols) => {
   const colored = cols.length * 2
   const switched = cols.reduce((total, value, i, values) => {
     const next = values[i+1]
-    if(next) if(value !== values[i+1]) total += 1
+    if(next) if(value !== next) total += 1
     return total
   }, 0)
   return colored + switched
